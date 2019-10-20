32ed7090fe99d4ca9b86d9bdb6a37c549e72623b
yoshoku <yoshoku@outlook.com>
2019-10-21 00:59:33 +0900

    :sparkles: Add linear support vector regressor

---
+++ lib/rumale/svm/linear_svr.rb
@@ -0,0 +1,150 @@
+# frozen_string_literal: true
+
+require 'numo/libsvm'
+require 'rumale/base/base_estimator'
+require 'rumale/base/regressor'
+
+module Rumale
+  module SVM
+    # LinearSVR is a class that provides Rumale interface for Support Vector Regressor in LIBLINEAR.
+    #
+    # @example
+    #   estimator = Rumale::SVM::LinearSVR.new(reg_param: 1.0, random_seed: 1)
+    #   estimator.fit(training_samples, traininig_target_values)
+    #   results = estimator.predict(testing_samples)
+    class LinearSVR
+      include Base::BaseEstimator
+      include Base::Regressor
+
+      # Return the weight vector for LinearSVR.
+      # @return [Numo::DFloat] (shape: [n_classes, n_features])
+      attr_reader :weight_vec
+
+      # Return the bias term (a.k.a. intercept) for LinearSVR.
+      # @return [Numo::DFloat] (shape: [n_classes])
+      attr_reader :bias_term
+
+      # Create a new regressor with Support Vector Regressor.
+      #
+      # @param loss [String] The type of loss function ('squared_epsilon_insensitive' or 'epsilon_insensitive').
+      # @param dual [Boolean] The flag indicating whether to solve dual optimization problem.
+      #   When n_samples > n_features, dual = false is more preferable.
+      #   This parameter is ignored if loss = 'epsilon_insensitive'.
+      # @param reg_param [Float] The regularization parameter.
+      # @param epsilon [Float] The epsilon parameter in loss function of espsilon-svr.
+      # @param fit_bias [Boolean] The flag indicating whether to fit the bias term.
+      # @param bias_scale [Float] The scale of the bias term.
+      #   This parameter is ignored if fit_bias = false.
+      # @param tol [Float] The tolerance of termination criterion.
+      # @param verbose [Boolean] The flag indicating whether to output learning process message
+      # @param random_seed [Integer/Nil] The seed value using to initialize the random generator.
+      def initialize(loss: 'squared_epsilon_insensitive', dual: true, reg_param: 1.0, epsilon: 0.1,
+                     fit_bias: true, bias_scale: 1.0, tol: 1e-3, verbose: false, random_seed: nil)
+        check_params_string(loss: loss)
+        check_params_float(reg_param: reg_param, epsilon: epsilon, bias_scale: bias_scale, tol: tol)
+        check_params_boolean(dual: dual, fit_bias: fit_bias, verbose: verbose)
+        check_params_type_or_nil(Integer, random_seed: random_seed)
+        @params = {}
+        @params[:loss] = loss == 'epsilon_insensitive' ? 'epsilon_insensitive' : 'squared_epsilon_insensitive'
+        @params[:dual] = dual
+        @params[:reg_param] = reg_param
+        @params[:epsilon] = epsilon
+        @params[:fit_bias] = fit_bias
+        @params[:bias_scale] = bias_scale
+        @params[:tol] = tol
+        @params[:verbose] = verbose
+        @params[:random_seed] = random_seed
+        @model = nil
+        @weight_vec = nil
+        @bias_term = nil
+      end
+
+      # Fit the model with given training data.
+      #
+      # @param x [Numo::DFloat] (shape: [n_samples, n_features]) The training data to be used for fitting the model.
+      # @param y [Numo::DFloat] (shape: [n_samples]) The target values to be used for fitting the model.
+      # @return [LinearSVR] The learned regressor itself.
+      def fit(x, y)
+        check_sample_array(x)
+        check_tvalue_array(y)
+        check_sample_tvalue_size(x, y)
+        xx = fit_bias? ? expand_feature(x) : x
+        @model = Numo::Liblinear.train(xx, y, liblinear_params)
+        @weight_vec, @bias_term = weight_and_bias(@model[:w])
+        self
+      end
+
+      # Predict values for samples.
+      #
+      # @param x [Numo::DFloat] (shape: [n_samples, n_features]) The samples to predict the labels.
+      # @return [Numo::DFloat] (shape: [n_samples]) Predicted value per sample.
+      def predict(x)
+        check_sample_array(x)
+        xx = fit_bias? ? expand_feature(x) : x
+        Numo::Liblinear.predict(xx, liblinear_params, @model)
+      end
+
+      # Dump marshal data.
+      # @return [Hash] The marshal data about LinearSVR.
+      def marshal_dump
+        { params: @params,
+          model: @model,
+          weight_vec: @weight_vec,
+          bias_term: @bias_term }
+      end
+
+      # Load marshal data.
+      # @return [nil]
+      def marshal_load(obj)
+        @params = obj[:params]
+        @model = obj[:model]
+        @weight_vec = obj[:weight_vec]
+        @bias_term = obj[:bias_term]
+        nil
+      end
+
+      private
+
+      def expand_feature(x)
+        n_samples = x.shape[0]
+        Numo::NArray.hstack([x, Numo::DFloat.ones([n_samples, 1]) * bias_scale])
+      end
+
+      def weight_and_bias(base_weight)
+        bias_vec = 0.0
+        weight_mat = base_weight.dup
+        if fit_bias?
+          bias_vec = weight_mat[-1]
+          weight_mat = weight_mat[0...-1].dup
+        end
+        [weight_mat, bias_vec]
+      end
+
+      def liblinear_params
+        res = {}
+        res[:solver_type] = solver_type
+        res[:eps] = @params[:tol]
+        res[:C] = @params[:reg_param]
+        res[:p] = @params[:epsilon]
+        res[:verbose] = @params[:verbose]
+        res[:random_seed] = @params[:random_seed]
+        res
+      end
+
+      def solver_type
+        return Numo::Liblinear::SolverType::L2R_L1LOSS_SVR_DUAL if @params[:loss] == 'epsilon_insensitive'
+        return Numo::Liblinear::SolverType::L2R_L2LOSS_SVR_DUAL if @params[:dual]
+
+        Numo::Liblinear::SolverType::L2R_L2LOSS_SVR
+      end
+
+      def fit_bias?
+        @params[:fit_bias]
+      end
+
+      def bias_scale
+        @params[:bias_scale]
+      end
+    end
+  end
+end
---
+++ spec/rumale/svm/linear_svr_spec.rb
@@ -0,0 +1,86 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Rumale::SVM::LinearSVR do
+  let(:dataset) { three_blobs }
+  let(:x) { Rumale::PairwiseMetric.linear_kernel(dataset[0]) }
+  let(:y) { dataset[0].dot(Rumale::Utils.rand_normal([2, 1], Random.new(1))).flatten }
+  let(:n_samples) { x.shape[0] }
+  let(:n_features) { x.shape[1] }
+  let(:loss) { 'squared_epsilon_insensitive' }
+  let(:dual) { false }
+  let(:fit_bias) { true }
+  let(:svr) { described_class.new(loss: loss, dual: dual, fit_bias: fit_bias, random_seed: 1) }
+  let(:predicted) { svr.predict(x) }
+  let(:copied) { Marshal.load(Marshal.dump(svr)) }
+
+  shared_examples 'regression task' do
+
+    before { svr.fit(x, y) }
+
+    it 'evaluates regression performance' do
+      expect(svr.weight_vec.class).to eq(Numo::DFloat)
+      expect(svr.weight_vec.ndim).to eq(1)
+      expect(svr.weight_vec.shape[0]).to eq(n_features)
+      expect(svr.bias_term.class).to eq(Float)
+      expect(svr.score(x, y)).to be >= 0.95
+      expect(predicted.class).to eq(Numo::DFloat)
+      expect(predicted.ndim).to eq(1)
+      expect(predicted.shape[0]).to eq(n_samples)
+    end
+
+    it 'dumps and restores itself using Marshal module.' do
+      expect(copied.instance_variable_get(:@model)).to eq(svr.instance_variable_get(:@model))
+      expect(copied.instance_variable_get(:@prob_param)).to eq(svr.instance_variable_get(:@prob_param))
+      expect(copied.params).to eq(svr.params)
+      expect(copied.weight_vec).to eq(svr.weight_vec)
+      expect(copied.bias_term).to eq(svr.bias_term)
+      expect(copied.score(x, y)).to eq(svr.score(x, y))
+    end
+  end
+
+  shared_examples 'hold out' do
+    let(:split) { Rumale::ModelSelection::KFold.new(n_splits: 5, shuffle: true, random_seed: 1).split(x, y).first }
+    let(:train_id) { split[0] }
+    let(:valid_id) { split[1] }
+    let(:x_train) { x[train_id, true] }
+    let(:y_train) { y[train_id] }
+    let(:x_valid) { x[valid_id, true] }
+    let(:y_valid) { y[valid_id] }
+
+    before { svr.fit(x_train, y_train) }
+
+    it 'evaluates classification performance' do
+      expect(svr.score(x_valid, y_valid)).to be >= 0.95
+    end
+  end
+
+  context 'when selected squared epsilon insensitive loss' do
+    let(:loss) { 'squared_epsilon_insensitive' }
+
+    it_behaves_like 'regression task'
+    it_behaves_like 'hold out'
+  end
+
+  context 'when selected epsilon insensitive loss' do
+    let(:loss) { 'epsilon_insensitive' }
+
+    it_behaves_like 'regression task'
+    it_behaves_like 'hold out'
+  end
+
+  context 'when selected dual solver' do
+    let(:dual) { true }
+
+    it_behaves_like 'regression task'
+    it_behaves_like 'hold out'
+  end
+
+  context 'when not fitting bias' do
+    let(:bias_term) { false }
+
+    it_behaves_like 'regression task'
+    it_behaves_like 'hold out'
+  end
+end
