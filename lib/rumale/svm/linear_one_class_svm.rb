75d00481cdd23178fc04d01e7ae4263a11590079
yoshoku <yoshoku@outlook.com>
2020-08-14 18:02:04 +0900

    :sparkles: Add linear one-class support vector machine

---
+++ lib/rumale/svm/linear_one_class_svm.rb
@@ -0,0 +1,121 @@
+# frozen_string_literal: true
+
+require 'numo/liblinear'
+require 'rumale/base/base_estimator'
+require 'rumale/validation'
+
+module Rumale
+  module SVM
+    # LinearOneClassSVM is a class that provides linear One-class Support Vector Machine in LIBLINEAR with Rumale interface.
+    #
+    # @example
+    #   estimator = Rumale::SVM::LinearOneClassSVM.new(nu: 0.05, random_seed: 1)
+    #   estimator.fit(training_samples, traininig_labels)
+    #   results = estimator.predict(testing_samples)
+    class LinearOneClassSVM
+      include Base::BaseEstimator
+      include Validation
+
+      # Return the weight vector for LinearOneClassSVM.
+      # @return [Numo::DFloat] (shape: [n_features])
+      attr_reader :weight_vec
+
+      # Return the bias term (a.k.a. intercept) for LinearOneClassSVM.
+      # @return [Float]
+      attr_reader :bias_term
+
+      # Create a new estimator with linear One-class Support Vector Machine.
+      #
+      # @param nu [Float] The fraction of data as outliers. The interval of nu is (0, 1].
+      # @param reg_param [Float] The regularization parameter.
+      # @param tol [Float] The tolerance of termination criterion.
+      # @param verbose [Boolean] The flag indicating whether to output learning process message
+      # @param random_seed [Integer/Nil] The seed value using to initialize the random generator.
+      def initialize(nu: 0.05, reg_param: 1.0, tol: 1e-3, verbose: false, random_seed: nil)
+        check_params_numeric(nu: nu, reg_param: reg_param, tol: tol)
+        check_params_boolean(verbose: verbose)
+        check_params_numeric_or_nil(random_seed: random_seed)
+        @params = {}
+        @params[:nu] = nu.to_f
+        @params[:reg_param] = reg_param.to_f
+        @params[:tol] = tol.to_f
+        @params[:verbose] = verbose
+        @params[:random_seed] = random_seed.nil? ? nil : random_seed.to_i
+        @weight_vec = nil
+        @bias_term = nil
+        @model = nil
+      end
+
+      # Fit the model with given training data.
+      #
+      # @overload fit(x) -> LinearOneClassSVM
+      #   @param x [Numo::DFloat] (shape: [n_samples, n_features]) The training data to be used for fitting the model.
+      #
+      # @return [LinearOneClassSVM] The learned estimator itself.
+      def fit(x, _y = nil)
+        x = check_convert_sample_array(x)
+        dummy = Numo::DFloat.ones(x.shape[0])
+        @model = Numo::Liblinear.train(x, dummy, liblinear_params)
+        @weight_vec = @model[:w].dup
+        @bias_term = @model[:rho]
+        self
+      end
+
+      # Calculate confidence scores for samples.
+      #
+      # @param x [Numo::DFloat] (shape: [n_samples, n_features]) The samples to compute the scores.
+      # @return [Numo::DFloat] (shape: [n_samples, n_classes]) Confidence score per sample.
+      def decision_function(x)
+        raise "#{self.class.name}\##{__method__} expects to be called after training the model with the fit method." unless trained?
+        x = check_convert_sample_array(x)
+        Numo::Liblinear.decision_function(x, liblinear_params, @model)
+      end
+
+      # Predict class labels for samples.
+      #
+      # @param x [Numo::DFloat] (shape: [n_samples, n_features]) The samples to predict the labels.
+      # @return [Numo::Int32] (shape: [n_samples]) Predicted label per sample.
+      def predict(x)
+        raise "#{self.class.name}\##{__method__} expects to be called after training the model with the fit method." unless trained?
+        x = check_convert_sample_array(x)
+        Numo::Int32.cast(Numo::Liblinear.predict(x, liblinear_params, @model))
+      end
+
+      # Dump marshal data.
+      # @return [Hash] The marshal data about LinearOneClassSVM.
+      def marshal_dump
+        { params: @params,
+          model: @model,
+          weight_vec: @weight_vec,
+          bias_term: @bias_term }
+      end
+
+      # Load marshal data.
+      # @return [nil]
+      def marshal_load(obj)
+        @params = obj[:params]
+        @model = obj[:model]
+        @weight_vec = obj[:weight_vec]
+        @bias_term = obj[:bias_term]
+        nil
+      end
+
+      private
+
+      def liblinear_params
+        res = {}
+        res[:solver_type] = Numo::Liblinear::SolverType::ONECLASS_SVM
+        res[:eps] = @params[:tol]
+        res[:C] = @params[:reg_param]
+        res[:nu] = @params[:nu]
+        res[:verbose] = @params[:verbose]
+        res[:random_seed] = @params[:random_seed]
+        res
+      end
+
+      def trained?
+        !@model.nil?
+      end
+    end
+  end
+end
---
+++ spec/rumale/svm/linear_one_class_svm_spec.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Rumale::SVM::LinearOneClassSVM do
+  let(:x_pos) { Rumale::Utils.rand_normal([980, 2], Random.new(1), 8.0, 1.0) }
+  let(:x_neg) { Rumale::Utils.rand_normal([20, 2], Random.new(1), 0.0, 0.1) }
+  let(:x) { Numo::NArray.vstack([x_pos, x_neg]) }
+  let(:n_samples) { x.shape[0] }
+  let(:n_features) { x.shape[1] }
+  let(:n_neg_samples) { x_neg.shape[0] }
+  let(:nu) { 0.01 }
+  let(:ocsvm) { described_class.new(nu: nu, random_seed: 1) }
+  let(:copied) { Marshal.load(Marshal.dump(ocsvm)) }
+
+  describe 'outlier detection task' do
+    let(:dfs) { ocsvm.decision_function(x) }
+    let(:predicted) { ocsvm.predict(x) }
+
+    before { ocsvm.fit(x_pos) }
+
+    it 'evaluates performance' do
+      expect(ocsvm.weight_vec.class).to eq(Numo::DFloat)
+      expect(ocsvm.weight_vec.ndim).to eq(1)
+      expect(ocsvm.weight_vec.shape[0]).to eq(n_features)
+      expect(ocsvm.bias_term.class).to eq(Float)
+      expect(predicted.class).to eq(Numo::Int32)
+      expect(predicted.ndim).to eq(1)
+      expect(predicted.shape[0]).to eq(n_samples)
+      expect(predicted.eq(1).count).to be >= 970
+      expect(predicted.eq(-1).count).to be <= 30
+    end
+
+    it 'calculates decision function values' do
+      expect(dfs.class).to eq(Numo::DFloat)
+      expect(dfs.ndim).to eq(1)
+      expect(dfs.shape[0]).to eq(n_samples)
+      expect(dfs.gt(0).count).to eq(predicted.eq(1).count)
+    end
+
+    it 'dumps and restores itself using Marshal module.' do
+      expect(copied.instance_variable_get(:@model)).to eq(ocsvm.instance_variable_get(:@model))
+      expect(copied.params).to eq(ocsvm.params)
+    end
+  end
+
+  context 'when called predict method before training with fit method' do
+    it 'raises Runtime error' do
+      expect { ocsvm.predict(x) }.to raise_error(
+        RuntimeError, 'Rumale::SVM::LinearOneClassSVM#predict expects to be called after training the model with the fit method.'
+      )
+      expect { ocsvm.decision_function(x) }.to raise_error(
+        RuntimeError, 'Rumale::SVM::LinearOneClassSVM#decision_function expects to be called after training the model with the fit method.'
+      )
+    end
+  end
+end
